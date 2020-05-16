4a9cc54ec4989f7cc34fb0c2db2cc1bf2b421180
Erik Carlson <carlsonerikw@gmail.com>
2020-05-16 08:31:40 -0400

    Add initial modal and column structure

--- src/components/cart-checkout/index.js
+++ src/components/cart-checkout/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Modal = styled.div`
+  width: fit-content;
+  display: flex;
+  border: 1px #d0d0d0 solid;
+  border-radius: 0.25rem;
+  -webkit-box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.20);
+  -moz-box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.20);
+  box-shadow: 2px 2px 5px 0px rgba(0,0,0,0.20);
+`;
+
+const Column = styled.div`
+padding: 1rem;
+`;
 
 const CartCheckout = (props) => {
   return (
-    <div>cart checkout component</div>
+    <Modal>
+      <Column>left side</Column>
+      <Column>right side</Column>
+    </Modal>
   )
 }
 
